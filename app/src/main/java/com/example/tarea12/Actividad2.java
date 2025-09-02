ad61ef0941a0ee865d22d7bd14028c00939fc604
Martin Bernabe Alarcon Contreras <xmartinalarx@gmail.com>
2025-09-02 15:58:27 -0400

    Prueba commit

--- app/src/main/java/com/example/tarea12/Actividad2.java
+++ app/src/main/java/com/example/tarea12/Actividad2.java
@@ -11,7 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 // Martin Alarcon, Martin Araya, Julian Ojeda
-
+//prueba
 public class Actividad2 extends AppCompatActivity {
 
     @Override
